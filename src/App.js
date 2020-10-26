8932738763393b3abcd11eb50ab873c41cb6242a
DESKTOP-SGAB5U8\Rachel <rachelstuckey@yahoo.com>
2020-10-26 15:49:37 -0700

    not entirely working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,69 @@
 import React from 'react';
+import ToDoForm from './components/TodoForm';
+import ToDoList from './components/TodoList';
+
+const list = [];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+       list 
+    }
+  }
+
+  handleToggleItem = (itemId) => {
+
+    this.setState({
+      list: this.state.list.map(item => {
+        if (item.id === itemId) {
+          return {
+            ...item,
+            done: !item.done
+          }
+        }
+        else {
+          return item;
+        }
+      })
+    })
+  }
+  
+  handleAddItem = (name) => {
+    console.log('handleAddItem')
+    this.setState({
+      list: [
+        ...this.state.list, {
+          name: name,
+          id: this.state.list.length,
+          done: false
+        }
+      ]
+    })
+  }
+
+  clearItems = () => {
+    this.setState({
+      list: this.state.list.filter(item => (!item.done))
+    });
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <ToDoForm
+          handleAddItem={this.handleAddItem}
+        />
+
+        <ToDoList
+          handleClearItems={this.clearItems}
+          handleToggleItem={this.handleToggleItem}
+          list={this.state.list}
+        />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,38 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+import Item from './Item';
+
+const ToDoList = props => {
+    const { list } = props;
+  
+    const handleClick = () => {
+        props.handleClearItems()
+    }
+
+   
+    return (
+        // <div onClick={this.handleClick} className={`item${props.item.purchased ? ' purchased' : ''}`}>
+        <div onClick={handleClick}>
+            <h2>This is ToDoList</h2>
+            {
+                list.map(item => (
+                    <Item
+                        handleToggleItem={props.handleToggleItem}
+                        key={item.id}
+                        item={item}
+                    />
+                ))
+            }
+            <button
+                onClick={handleClick}
+            >
+                Clear Done
+            </button>
+        </div>
+    );
+    
+}
+
+export default ToDoList;
